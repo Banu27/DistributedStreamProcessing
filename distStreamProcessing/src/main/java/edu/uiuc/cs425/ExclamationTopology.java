58ac49f5f5df1bdc06aeb71a019be76e76e077e8
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-12-06 01:07:50 -0600

    exclamation spout paralleism 3

--- distStreamProcessing/src/main/java/edu/uiuc/cs425/ExclamationTopology.java
+++ distStreamProcessing/src/main/java/edu/uiuc/cs425/ExclamationTopology.java
@@ -4,7 +4,7 @@ public class ExclamationTopology implements ITopologyDefn{
 
 	public Topology CreateTopology() {
 		TopologyBuilder builder = new TopologyBuilder("ExclamationTopology");
-		builder.setSpout("ExclamationSpout", "edu.uiuc.cs425.ExclamationSpout", 6);
+		builder.setSpout("ExclamationSpout", "edu.uiuc.cs425.ExclamationSpout", 3);
 		builder.setBolt("ExclamationBolt1", "edu.uiuc.cs425.ExclamationBolt","ExclamationSpout",Commons.GROUPING_SHUFFLE,
 				null,2);
 		builder.setBolt("ExclamationBolt2", "edu.uiuc.cs425.ExclamationBolt","ExclamationBolt1",Commons.GROUPING_SHUFFLE,
