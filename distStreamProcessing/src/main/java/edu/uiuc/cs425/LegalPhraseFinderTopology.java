3f515d3352a2adf856e9ceb616dd684dd79feeaa
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-12-06 23:52:04 -0600

    fix for less tuples to a node

--- distStreamProcessing/src/main/java/edu/uiuc/cs425/LegalPhraseFinderTopology.java
+++ distStreamProcessing/src/main/java/edu/uiuc/cs425/LegalPhraseFinderTopology.java
@@ -4,11 +4,11 @@ public class LegalPhraseFinderTopology implements ITopologyDefn {
 
 	public Topology CreateTopology() {
 		TopologyBuilder builder = new TopologyBuilder("LegalPhraseFinderTopology");
-		builder.setSpout("SentenceGen", "edu.uiuc.cs425.GenSentenceFileSpout", 4);
+		builder.setSpout("SentenceGen", "edu.uiuc.cs425.GenSentenceFileSpout", 3);
 		builder.setBolt("SplitSentence", "edu.uiuc.cs425.SplitSentenceBolt","SentenceGen",Commons.GROUPING_SHUFFLE,
-				null,4);
+				null,3);
 		builder.setBolt("PhraseCounter", "edu.uiuc.cs425.LegalPhraseFinderBolt","SplitSentence",Commons.GROUPING_FIELD,
-				"word",4);
+				"word",3);
 		
 		return builder.GetTopology();
 	}
