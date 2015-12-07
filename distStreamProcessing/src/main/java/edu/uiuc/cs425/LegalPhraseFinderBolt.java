51d573942c8b31376ba9548f3c4502b53c7a1919
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-12-06 18:24:58 -0600

    new application files

---
+++ distStreamProcessing/src/main/java/edu/uiuc/cs425/LegalPhraseFinderBolt.java
@@ -0,0 +1,10 @@
+package edu.uiuc.cs425;
+
+public class LegalPhraseFinderBolt extends BComponent implements IBolt {
+
+	public void execute(Tuple tuple) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
